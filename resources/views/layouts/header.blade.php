a83e1c9b1035b00f525e1e7ff1f7d14671f1c87e
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-04-24 02:13:15 +0700

    templating

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html class="bootstrap-layout">
+
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Dashboard</title>
+
+  <!-- Prevent the demo from appearing in search engines (REMOVE THIS) -->
+  <meta name="robots" content="noindex">
+
+  <!-- Material Design Icons  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+  <!-- Roboto Web Font -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en" rel="stylesheet">
+
+  <!-- App CSS -->
+  <link type="text/css" href="{{ asset(null) }}assets/css/style.min.css" rel="stylesheet">
+
+</head>
+
+<body class="layout-container ls-top-navbar si-l3-md-up">
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-dark bg-primary navbar-full navbar-fixed-top">
+
+    <!-- Toggle sidebar -->
+    <button class="navbar-toggler pull-xs-left" type="button" data-toggle="sidebar" data-target="#sidebarLeft"><span class="material-icons">menu</span></button>
+
+    <!-- Brand -->
+    <a href="index-2.html" class="navbar-brand"><i class="material-icons">school</i> LearnPlus</a>
+
+    <!-- Search -->
+    <form class="form-inline pull-xs-left hidden-sm-down">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search">
+        <span class="input-group-btn"><button class="btn" type="button"><i class="material-icons">search</i></button></span>
+      </div>
+    </form>
+    <!-- // END Search -->
+
+    <ul class="nav navbar-nav hidden-sm-down">
+
+      <!-- Menu -->
+      <li class="nav-item">
+        <a class="nav-link" href="forum.html">Forum</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="get-help.html">Get Help</a>
+      </li>
+    </ul>
+
+    <!-- Menu -->
+    <ul class="nav navbar-nav pull-xs-right">
+
+      <li class="nav-item">
+        <a href="cart.html" class="nav-link">
+          <i class="material-icons">shopping_cart</i>
+        </a>
+      </li>
+
+      <!-- User dropdown -->
+      <li class="nav-item dropdown">
+        <a class="nav-link active dropdown-toggle p-a-0" data-toggle="dropdown" href="#" role="button" aria-haspopup="false">
+          <img src="{{ asset(null) }}assets/images/people/50/guy-6.jpg" alt="Avatar" class="img-circle" width="40">
+        </a>
+        <div class="dropdown-menu dropdown-menu-right dropdown-menu-list" aria-labelledby="Preview">
+          <a class="dropdown-item" href="account-edit.html"><i class="material-icons md-18">lock</i> <span class="icon-text">Edit Account</span></a>
+          <a class="dropdown-item" href="profile.html"><i class="material-icons md-18">person</i> <span class="icon-text">Public Profile</span></a>
+          <a class="dropdown-item" href="login.html">Logout</a>
+        </div>
+      </li>
+      <!-- // END User dropdown -->
+
+    </ul>
+    <!-- // END Menu -->
+
+  </nav>
