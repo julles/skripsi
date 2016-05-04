58665f2e3c7c23da1fab2f8de19774e695ae32c5
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-05-04 22:52:59 +0700

    add team controller and capsule_setting file

--- app/Http/Controllers/TeamController.php
+++ app/Http/Controllers/TeamController.php
@@ -6,8 +6,23 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Models\Team;
+
 class TeamController extends Controller
 {
+	public $model;
+
+	public function __construct(Team $team)
+	{
+		$this->team = $team;
+	}
+
+	/**
+	 * Method Listing Team
+	 * Menampilkan team yang di miliki si owner , atau yang terkait
+	 * dengan team lain
+	 */
+
     public function getIndex()
     {
     	
---
+++ config/capsule_setting.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'application_name'	=> 'Task',
+	'domain'			=> 'task.com',
+];
